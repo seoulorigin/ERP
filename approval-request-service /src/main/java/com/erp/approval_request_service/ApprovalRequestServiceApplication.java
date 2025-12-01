9eb9898e4cd977959a139dc632cdbdc647ea84a6
seoulorigin <seoulorigin@gmail.com>
2025-12-01 11:58:47 +0000

    chore: Setup initial project structure for Approval Request Service

---
+++ approval-request-service /src/main/java/com/erp/approval_request_service/ApprovalRequestServiceApplication.java
@@ -0,0 +1,13 @@
+package com.erp.approval_request_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApprovalRequestServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApprovalRequestServiceApplication.class, args);
+	}
+
+}
---
+++ approval-request-service /src/test/java/com/erp/approval_request_service/ApprovalRequestServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.erp.approval_request_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApprovalRequestServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
